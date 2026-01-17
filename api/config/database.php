270b83a4c5f4071f100c998ac35bd00a1e129af4
LebohangMonamane <monamane.lebohang45@gmail.com>
2026-01-17 11:53:47 +0200

    done

--- api/config/database.php
+++ api/config/database.php
@@ -44,9 +44,7 @@ class Database {
                 [
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                    PDO::ATTR_EMULATE_PREPARES => false,
-                    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
-                    PDO::ATTR_PERSISTENT => true
+                    PDO::ATTR_EMULATE_PREPARES => false
                 ]
             );
         } catch(PDOException $exception) {
