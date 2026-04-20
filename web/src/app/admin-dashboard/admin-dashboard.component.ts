c313de044d2986d215b95375a9c90c4d3b1513f2
LebohangMonamane <lebohang@bsc.africa>
2026-04-20 17:55:46 +0200

    done

--- web/src/app/admin-dashboard/admin-dashboard.component.ts
+++ web/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @Component({
   selector: 'app-admin-dashboard',
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
